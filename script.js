4587f99e166a44f107748ac18722411ba16cd62c
Magnus Nilsson <regnhundar@gmail.com>
2023-12-19 16:41:29 +0100

    glömde lägga till filer

---
+++ script.js
@@ -0,0 +1,34 @@
+// G-Version - Split the Nota
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+
+SET variabel summa = användarinput
+SET variabel antalPersoner = användarinput
+SET dricks = användarinput
+SET resultat = 0;
+
+function uträkning()
+    while resultat >=0
+
+*/
+
+// VG-Version - Lewis Carroll Word Puzzle
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+
+function play()
+    SET variabel ordbok = [..., ..., ...]; // Innehåller ALLA ord i det engelska språket
+    SET variabel startOrd till "FOUR";
+    SET variabel slutordOrd till "FIVE";
+
+
+end function
+
+function isOneLetterApart(wordOne, wordTwo)
+    SET variabel diffCount till 0;
+
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+end function
+
+*/
